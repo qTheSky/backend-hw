a88622321c0cd740e765d5c86e65dbfff5d285e2
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-12-06 17:55:31 +0300

    1

--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,17 @@ app.post('/videos', (req: Request, res: Response) => {
 						}
 				]
 		}
+		if (!req.body.title.trim().length) {
+				validation.errorsMessages[0].message = 'title should be in body'
+				validation.errorsMessages[0].field = 'title'
+				res.status(400).json(validation)
+		}
+		if (!req.body.author.trim().length) {
+				validation.errorsMessages[0].message = 'author should be in body'
+				validation.errorsMessages[0].field = 'author'
+				res.status(400).json(validation)
+		}
+
 		if (req.body.author.length > 20) {
 				validation.errorsMessages[0].message = 'author length must me less than 20 symbols'
 				validation.errorsMessages[0].field = 'author'
