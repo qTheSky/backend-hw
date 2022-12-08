f9d4c0355291f6096d3bea0b5bd8d78eaee708cd
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-12-08 16:31:54 +0300

    validation

--- src/index.ts
+++ src/index.ts
@@ -19,18 +19,18 @@ interface Video {
 }
 
 let videos: Video[] = [
-		// {
-		// 		id: 0,
-		// 		title: 'string',
-		// 		author: 'string',
-		// 		canBeDownloaded: true,
-		// 		minAgeRestriction: null,
-		// 		createdAt: '2022-12-06T13:23:30.896Z',
-		// 		publicationDate: '2022-12-06T13:23:30.896Z',
-		// 		availableResolutions: [
-		// 				'P144'
-		// 		]
-		// }
+		{
+				id: 0,
+				title: 'string',
+				author: 'string',
+				canBeDownloaded: true,
+				minAgeRestriction: null,
+				createdAt: '2022-12-06T13:23:30.896Z',
+				publicationDate: '2022-12-06T13:23:30.896Z',
+				availableResolutions: [
+						'P144'
+				]
+		}
 ]
 
 app.delete('/testing/all-data', (req: Request, res: Response) => {
@@ -50,6 +50,21 @@ app.get('/videos/:id', (req: Request, res: Response) => {
 		}
 })
 app.post('/videos', (req: Request, res: Response) => {
+		const validation = {
+				errorMessages: [] as Array<{ message: string, field: string }>
+		}
+
+		if (!req.body.title || req.body.title.trim().length > 40) {
+				validation.errorMessages.push({message: 'title should exist and < 40 symbols', field: 'title'})
+		}
+		if (!req.body.author || req.body.author.trim().length > 20) {
+				validation.errorMessages.push({message: 'author should exist and < 20 symbols', field: 'author'})
+		}
+		if (validation.errorMessages.length) {
+				res.status(400).send(validation)
+				return
+		}
+
 		const createdVideo: Video = {
 				author: req.body.author,
 				title: req.body.title,
@@ -64,6 +79,20 @@ app.post('/videos', (req: Request, res: Response) => {
 		res.status(201).send(createdVideo)
 })
 app.put('/videos/:id', (req: Request, res: Response) => {
+		const validation = {
+				errorMessages: [] as Array<{ message: string, field: string }>
+		}
+
+		if (!req.body.title || req.body.title.trim().length > 40) {
+				validation.errorMessages.push({message: 'title should exist and < 40 symbols', field: 'title'})
+		}
+		if (!req.body.author || req.body.author.trim().length > 20) {
+				validation.errorMessages.push({message: 'author should exist and < 20 symbols', field: 'author'})
+		}
+		if (validation.errorMessages.length) {
+				res.status(400).send(validation)
+				return
+		}
 
 		const foundVideo = videos.find(v => v.id === +req.params.id)
 		if (foundVideo) {
